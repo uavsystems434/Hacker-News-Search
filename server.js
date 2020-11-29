a6fb2cd29efcb6ce27330a9bf8d920d75495babc
uavsystems434 <71169299+uavsystems434@users.noreply.github.com>
2020-11-29 00:46:13 -0500

    text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set( 'port', ( process.env.PORT || 5000 ));
+app.set( 'port', ( process.env.PORT || 8080 ));
 
 // Start node server
 app.listen( app.get( 'port' ), function() {
