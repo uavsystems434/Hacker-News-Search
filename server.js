41f07e8e88efb850b81acf54544191b16142ee37
uavsystems434 <71169299+uavsystems434@users.noreply.github.com>
2020-11-29 01:12:21 -0500

    get

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/src', function (req, res) {
